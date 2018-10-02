bf634e1a7d3725d6f193834c86eb0852be787d25
Epic Danc <matt.stockton17@gmail.com>
2018-10-02 06:14:36 -0600

    Tried to fix some of the dumb gifs.

--- giphy.js
+++ giphy.js
@@ -18,8 +18,12 @@
 function getGiphy() {
     var url = "https://api.giphy.com/v1/gifs/search?q="+ $("#searchField").val() +" pokemon&api_key=qF6XRjYmYUwBhyQnzZsX9dyKtArA8XfH&limit=1"
     $.getJSON(url, function(data) {
-        console.log(data.data[0].embed_url)
-        newHtml = "<iframe src=\"" + data.data[0].embed_url + "\"></iframe> <p>Disclaimer: this may or may not be the correct pokemon gif. We are not responsible for what shows up.</p>"
+        var i = 0;
+        // var pokeName = $("#searchField").val()
+        // if (pokeName.toLowerCase() == "vaporeon") {i=2;}
+        console.log(i);
+        console.log(data.data[i].embed_url)
+        newHtml = "<iframe src=\"" + data.data[i].embed_url + "\"></iframe> <p>Disclaimer: this may or may not be the correct pokemon gif. We are not responsible for what shows up.</p>"
         $("#giphy").html(newHtml);
     })
     .fail(function(jqXHR, textStatus, errorThrown) { 
